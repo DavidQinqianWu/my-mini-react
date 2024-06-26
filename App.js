06fd0bd181ab43413ea3cd3f9a12e199fa8a1b04
DavidQinqianWu <wqq1992324@gmail.com>
2024-06-26 23:45:18 +0800

    impletement jsx

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from './core/React.js';
+
+const App = React.createElement(
+  'div',
+  { id: 'app' },
+  'hello app123',
+  'woshidierege',
+  '123123123123'
+);
+
+export default App;
